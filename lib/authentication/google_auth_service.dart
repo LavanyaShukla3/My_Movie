2996e8fff5f2638b4875efaed2c12dbbf36dceab
Lavanya Shukla <112153884+LavanyaShukla3@users.noreply.github.com>
2023-10-26 15:01:17 +0530

    Update google_auth_service.dart

--- lib/authentication/google_auth_service.dart
+++ lib/authentication/google_auth_service.dart
@@ -1,31 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import '../movie_db/movie_homepage.dart';
 
-class AuthService {
+
+class AuthService{
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  Future<UserCredential?> googleSignIn() async {
-    try {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+  Future<void> handleGoogleSignIn(BuildContext context) async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+    if (googleUser != null) {
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
-      return userCredential;
-    } catch (e) {
-      print('Error signing in with Google: $e');
-      return null;
-    }
-  }
 
-  void signOut() async {
-    await _auth.signOut();
-    await _googleSignIn.signOut();
+      final UserCredential authResult = await FirebaseAuth.instance
+          .signInWithCredential(credential);
+      final User? user = authResult.user;
+
+      if (user != null) {
+        // Authentication successful, navigate to MovieHomepage
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MovieHomepage(),
+          ),
+        );
+      }
+    }
+
+    signOut() async {
+      await _auth.signOut();
+      await _googleSignIn.signOut();
+    }
   }
 }
 
